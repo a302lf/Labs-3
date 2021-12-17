a8ce80bd1de10fc0656f9af46b59879ccfa698f4
a302lf <71495392+a302lf@users.noreply.github.com>
2021-12-18 02:21:44 +0300

    Create 2.1.py

---
+++ 2.1.py
@@ -0,0 +1,15 @@
+for x in range (6):
+	match x:
+		case 0:
+			print(f'Section № {x}')
+		case 1:
+			print(f'Section № {x}')
+		case 2:
+			print(f'Section № {x}')
+		case 3:
+			print(f'Section № {x}')
+		case 4:
+			print(f'Section № {x}')
+		case 5:
+			print(f'Section № {x}')
+    
