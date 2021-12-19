47af1b5b65de46534c86251a2fcc406c82bf4e25
a302lf <71495392+a302lf@users.noreply.github.com>
2021-12-19 04:37:39 +0300

    Update def_5.py

--- def_5.py
+++ def_5.py
@@ -1,6 +1,6 @@
 def unit (numb):
   match(numb):
-    case 0: pass
+    case 0: part =''
     case 1: part ='one'
     case 2: part ='two'
     case 3: part ='three'
@@ -12,9 +12,9 @@ def unit (numb):
     case 9: part ='nine'
   return part
 
-
 def dozen (numb):
   match(numb):
+    case 0: part =''
     case 2: part ='twenty'
     case 3: part ='thirty'
     case 4: part ='fourty'
@@ -24,7 +24,6 @@ def dozen (numb):
     case 8: part ='eighty'
     case 9: part ='ninety'
   return part
-
 
 def especial(numb):
   match(numb):
@@ -40,7 +39,6 @@ def especial(numb):
     case 9: part ='nineteen'
   return part
 
-
 def value(numb):
   match(numb):
     case 0: part ='hundred'
@@ -50,50 +48,52 @@ def value(numb):
     case 4: part ='trillion'
   return part
 
-
 def valency(fullnumb): 
   countnumb = 0
-  while fullnumb/10 > 0:
-    fullnumb=fullnumb%10
+  while fullnumb > 0:
+    fullnumb=fullnumb//10
     countnumb +=1
   return countnumb
-
 
 def decription(trio):
   decr = []
   first = trio//100
-  second = (trio%10)//10
+  second = (trio%100)//10
   third = trio%10
-  if first!=0:
-    decr.append(unit(first))
-  if first > 1:
+  decr.append(unit(first))
+  if second > 1:
     decr.append(dozen(second))
     decr.append(unit(third))
-  elif first == 1:
-    decr.append(especial(third))
-  else:
-    decr.append(unit(third))
-  return decr.reverse()
-
+  else: 
+    match(second):
+      case 1:
+        decr.append(especial(third))
+      case 0:
+        decr.append(unit(third))
+  return decr
 
 def add_value(trio, step, decr):
   if (trio>99):
-    decr.insert(-2, value(0))
+    decr.insert(1, value(0))
   if (step>3) and (trio!=0):
-    decr.append(value(step//3))
+    decr.append(value(step//3-1))
   return decr
 
-
 def full_decription(fullnumb):
-  fulldecript = []
-  countnumb = valency(fullnumb) + 1
-  for step in range(3, countnumb, 3):
+  fulldecr = []
+  strfulldecr = ''
+  if fullnumb==0: fulldecr.append('zero')
+  countnumb = valency(fullnumb) 
+  while (countnumb%3)!=0:
+    countnumb+=1
+  for step in range(3, countnumb+1, 3):
     decr = []
+    strdecr = ''
     trio = (fullnumb%(10**step))//(10**(step-3))
     decr = decription(trio) 
     decr = add_value(trio, step, decr)
-    fulldecript.insert(-1, decr)
-  return fulldecript
-
-
+    strdecr = ' '.join(decr)
+    fulldecr.insert(0,strdecr)
+  strfulldecr=' '.join(fulldecr)
+  return strfulldecr
 
